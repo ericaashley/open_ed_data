c4d4d715300167c596c4604fca75417bb7522dc2
emanoppo <emanoppo@users.noreply.github.com>
2015-04-15 13:22:14 -0400

    Fix Hound comments.

--- app/models/state.rb
+++ app/models/state.rb
@@ -2,9 +2,9 @@ class State < ActiveRecord::Base
   has_many :districts, foreign_key: "state_id"
   has_many :cities, foreign_key: "state_id"
   has_many :schools_in_districts, class_name: "School", through: :districts,
-    foreign_key: "ansi"
+            foreign_key: "ansi"
   has_many :schools_in_cities, class_name: "School", through: :cities,
-    foreign_key: "city_name"
+            foreign_key: "city_name"
 
   validates :ansi, presence: true
   validates :state_name, presence: true
