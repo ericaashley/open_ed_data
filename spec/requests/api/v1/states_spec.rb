88dcd642982c4df575e04738c9ce338b291192a9
emanoppo <emanoppo@users.noreply.github.com>
2015-04-20 15:40:18 -0400

    Add spec files for API (states).

---
+++ spec/requests/api/v1/states_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe "GET '/api/v1/states'" do
+  it 'retrieves a list of states' do
+    FactoryGirl.create_list(:state, 10)
+
+    get api_v1_states_path
+
+    expect(response).to be_success
+    json = JSON.parse(response.body)
+    expect(json['states'].length).to eq(10)
+  end
+end
+
+# describe "States API" do
+#   # it 'retrieves a specific message' do
+#   #   state = FactoryGirl.create(:state)
+#   #   get "/api/v1/states/#{state.id}"
+#   #
+#   #   # test for the 200 status-code
+#   #   expect(response).to be_success
+#   #
+#   #   # check that the message attributes are the same.
+#   #   expect(json['content']).to eq(message.content)
+#   #
+#   #   # ensure that private attributes aren't serialized
+#   #   expect(json['private_attr']).to eq(nil)
+#   # end
+# end
--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -14,4 +14,31 @@ FactoryGirl.define do
     provider 'facebook'
     sequence(:uid) { |n| n.to_s }
   end
+
+  factory :state do
+    sequence(:ansi_id) { |n| "ansi#{n}" }
+    sequence(:state_name) { |n| "State#{n}" }
+    sequence(:state_abbrev) { |n| "ST#{n}" }
+  end
+
+  factory :district do
+    association :state
+
+    sequence(:nces_id) { |n| "ID#{n}"}
+    sequence(:district_name) { |n| "District#{n}" }
+  end
+
+  factory :city do
+    association :state
+
+    sequence(:city_name) { |n| "City#{n}" }
+  end
+
+  factory :school do
+    association :district
+    association :city
+    association :state
+
+    sequence(:ncessch) { |n| "NCESSCH#{n}" }
+  end
 end
