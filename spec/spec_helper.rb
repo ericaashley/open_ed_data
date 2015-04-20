514c7fb6cc20a94c81955a1c7862172b0e46aa25
emanoppo <emanoppo@users.noreply.github.com>
2015-04-19 22:52:48 -0400

    Add initial API testing files for schools, districts, and states.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,7 @@ require 'support/omniauth_test_helper.rb'
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.include OmniauthTestHelper
+  config.include Requests::JsonHelpers, type: :request
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
