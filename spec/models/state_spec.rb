1b2fd361227f1989fce812c3dd9427a1a4fd49ec
emanoppo <emanoppo@users.noreply.github.com>
2015-04-22 21:44:43 -0400

    Update state_spec.rb file to match updated model.

--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe State, type: :model do
   it { should have_many(:districts) }
   it { should have_many(:cities) }
-  it { should have_many(:schools_in_districts).through(:districts) }
-  it { should have_many(:schools_in_cities).through(:cities) }
+  it { should have_many(:schools) }
   it { should validate_presence_of(:ansi_id) }
   it { should validate_presence_of(:state_name) }
   it { should validate_presence_of(:state_abbrev) }
